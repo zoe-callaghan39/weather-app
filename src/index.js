65b8e0e64af973e7953bb60182f9207cf22ee93b
zca01 <zoe.callaghan@skybettingandgaming.com>
2025-05-19 06:15:10 +0100

    removing service worker and offline page

--- src/index.js
+++ src/index.js
@@ -6,22 +6,6 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(<App />);
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', () => {
-    // Register the custom service worker located at public/service-worker.js
-    navigator.serviceWorker
-      .register(
-        `${window.location.origin}${window.location.pathname}service-worker.js`
-      )
-      .then(() => {
-        console.log('Service Worker registered successfully');
-      })
-      .catch((err) => {
-        console.error('Service Worker registration failed:', err);
-      });
-  });
-}
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
